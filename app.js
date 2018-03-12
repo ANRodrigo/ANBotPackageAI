29149cfbbc6c959458c370ff99331b3225e5f891
RodrigoCM3 <rodrigo.casiano@anglobal.com>
2018-03-12 16:54:22 -0600

    QNA integration

--- app.js
+++ app.js
@@ -41,8 +41,13 @@ var luisAppId = process.env.LuisAppId;
 var luisAPIKey = process.env.LuisAPIKey;
 var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
 
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
-
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/applicatio        n?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+// Maind  qna
+var qnarecognizer = new cognitiveservices.QnAMakerRecognizer({
+    knowledgeBaseId: process.env.QNAknowledgeBaseId,
+    subscriptionKey: process.env.QNAsubscriptionKey,
+    top: 4
+});
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 var intents = new builder.IntentDialog({ recognizers: [recognizer] })
@@ -55,11 +60,17 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
     .matches('Cancel', (session) => {
         session.send('You reached Cancel intent, you said qna \'%s\'.', session.message.text);
     })
+    .matches('qna', [
+        function(session, args, next) {
+            var answerEntity = builder.EntityRecognizer.findEntity(args.entities, 'answer');
+            session.send(answerEntity.entity);
+        }
+    ])
     /*
     .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
     */
     .onDefault((session) => {
-        session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+        session.send('Perdon no entiendo, podria reformular su pregunta \'%s\'.', session.message.text);
     });
 
 bot.dialog('/', intents);
